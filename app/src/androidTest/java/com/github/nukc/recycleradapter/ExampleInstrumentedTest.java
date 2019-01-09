42c16af2bcd0741434d4cbe9766c5166fe0f0162
nukc <353932158@qq.com>
2019-01-09 22:36:50 +0800

    test: update

--- app/src/androidTest/java/com/github/nukc/recycleradapter/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/github/nukc/recycleradapter/ExampleInstrumentedTest.java
@@ -1,8 +1,8 @@
 package com.github.nukc.recycleradapter;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.github.nukc.recycleradapter", appContext.getPackageName());
+        assertEquals("com.github.nukc.recycleradapter.sample", appContext.getPackageName());
     }
 }
